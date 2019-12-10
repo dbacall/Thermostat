83a6941f35a9db74a62da96275cbb8d4b4bda378
dbacall <dbacall@hotmail.co.uk>
2019-12-10 17:33:45 +0000

    add up method

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,7 +7,22 @@ describe("Thermostat", function() {
   });
 
   it("starts at 20 degrees", function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.currentTemp()).toEqual(20);
 
   });
+
+  describe('.up', function(){
+    
+    it('turns temperature up by 2', function(){
+      thermostat.up(2)
+      expect(thermostat.currentTemp()).toEqual(22)
+    });
+
+    it('turns temperature up by 5', function(){
+      thermostat.up(5)
+      expect(thermostat.currentTemp()).toEqual(25)
+    });
+    
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temperature = 20;
+}
+
+Thermostat.prototype.currentTemp = function(){
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function(temp){
+  this.temperature += temp
 };
