f454e76dff85068e148dac9aabad1974b58848a5
dbacall <dbacall@hotmail.co.uk>
2019-12-11 11:53:31 +0000

    add power saver, reset, usage and colour setter methods

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -13,16 +13,93 @@ describe("Thermostat", function() {
 
   describe('.up', function(){
     
-    it('turns temperature up by 2', function(){
-      thermostat.up(2)
-      expect(thermostat.currentTemp()).toEqual(22)
-    });
-
-    it('turns temperature up by 5', function(){
-      thermostat.up(5)
-      expect(thermostat.currentTemp()).toEqual(25)
+    it('turns temperature up by 1', function(){
+      thermostat.up()
+      expect(thermostat.currentTemp()).toEqual(21)
     });
     
+  });
+
+  describe('.down', function(){
+
+    it('turns temperature down by 1', function(){
+      thermostat.down()
+      expect(thermostat.currentTemp()).toEqual(19)
+    });
+
+    it('raises an error if temperature reaches 10', function(){
+      for(i = 0; i < 10; i++){
+        thermostat.down();
+      };
+      expect(function(){thermostat.down()}).toThrowError("Temperature already at minimum!")
+    });
+
+  });
+
+  describe('.powerSaverOn', function(){
+
+    it('sets max temperature to 25', function(){
+      thermostat.powerSaverOn()
+      for(i = 0; i < 5; i++){
+        thermostat.up();
+      };
+      expect(function(){thermostat.up()}).toThrowError("Temperature already at maximum!")
+    });
+
+    it('is on by default', function(){
+      thermostat.powerSaverOn()
+      for(i = 0; i < 5; i++){
+        thermostat.up();
+      };
+      expect(function(){thermostat.up()}).toThrowError("Temperature already at maximum!")
+    });
+
+  });
+
+  describe('.powerSaverOff', function(){
+
+    it('sets max temperature to 32', function(){
+      thermostat.powerSaverOff()
+      for(i = 0; i < 12; i++){
+        thermostat.up();
+      };
+      expect(function(){thermostat.up()}).toThrowError("Temperature already at maximum!")
+    });
+
+  });  
+
+  describe('.resetTemp', function(){
+
+    it('sets temperature to 20', function(){
+      thermostat.up()
+      expect(thermostat.currentTemp()).toEqual(21)
+      thermostat.resetTemp()
+      expect(thermostat.currentTemp()).toEqual(20)
+    });
+
+  })
+
+  describe('.isUsage', function(){
+    
+    it('returns low usage when below 18', function(){
+      for(i = 0; i < 3; i++){
+        thermostat.down();
+      };
+      expect(thermostat.isUsage()).toEqual("low-usage")
+    });
+
+    it('returns medium usage when between 18 and 25', function(){
+      expect(thermostat.isUsage()).toEqual("medium-usage")
+    });
+
+    it('returns high usage when 25 or higher', function(){
+      thermostat.powerSaverOff()
+      for(i = 0; i < 12; i++){
+        thermostat.up();
+      };
+      expect(thermostat.isUsage()).toEqual("high-usage")
+    });
+
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,57 @@
 function Thermostat() {
   this.temperature = 20;
+  this.powerSaving = true
+  this.max = 25
+  this.usageColour = "black"
 }
+
+const MINIMUM = 10
+
 
 Thermostat.prototype.currentTemp = function(){
   return this.temperature;
 };
 
-Thermostat.prototype.up = function(temp){
-  this.temperature += temp
+Thermostat.prototype.up = function(){
+  if(this.temperature == this.max){throw new Error("Temperature already at maximum!")}
+  this.temperature ++
+  this._colourSetter();
+};
+
+Thermostat.prototype.down = function(){
+  if (this.temperature == MINIMUM){throw new Error("Temperature already at minimum!")}
+  this.temperature --
+  this._colourSetter();
+};
+
+Thermostat.prototype.powerSaverOn = function(){
+  this.powerSaving = true
+  this.max = 25
+};
+
+Thermostat.prototype.powerSaverOff = function(){
+  this.powerSaving = true
+  this.max = 32
+};
+
+Thermostat.prototype.resetTemp = function(){
+  this.temperature = 20
+};
+
+Thermostat.prototype.isUsage = function(){
+  if (this.temperature < 18) {
+    return "low-usage";
+  } else if (this.temperature >= 18 && this.temperature < 25) {
+    return "medium-usage";
+  } else
+    return "high-usage";
+}
+
+Thermostat.prototype._colourSetter = function(){
+  if (this.temperature < 18) {
+    this.usageColour = 'green';
+  } else if (this.temperature >= 18 && this.temperature < 25) {
+    this.usageColour = 'black';
+  } else
+    this.usageColour = 'red';
 };
